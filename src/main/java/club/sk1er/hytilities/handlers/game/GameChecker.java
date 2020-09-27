a53a24422aebe25f30c410e6f4361af4e92b7603
chachy <38067923+ChachyDev@users.noreply.github.com>
2020-09-27 21:49:28 +0100

    new: implement rest of GameType (#4)

--- src/main/java/club/sk1er/hytilities/handlers/game/GameChecker.java
+++ src/main/java/club/sk1er/hytilities/handlers/game/GameChecker.java
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Handles checking for the current location of the player using many different techniques.
- *
  */
 
 public class GameChecker {
@@ -29,12 +28,54 @@ public class GameChecker {
             }
 
             if (!scoreboardTitle.isEmpty()) {
-                switch(scoreboardTitle) {
+                switch (scoreboardTitle) {
                     case "BED WARS":
                         gameType = GameType.BED_WARS;
                         break;
                     case "SKYWARS":
                         gameType = GameType.SKY_WARS;
+                        break;
+                    case "PROTOTYPE":
+                        gameType = GameType.PROTOTYPE;
+                        break;
+                    case "MURDER MYSTERY":
+                        gameType = GameType.MURDER_MYSTERY;
+                        break;
+                    case "HOUSING":
+                        gameType = GameType.HOUSING;
+                        break;
+                    case "ARCADE GAMES":
+                        gameType = GameType.ARCADE_GAMES;
+                        break;
+                    case "BUILD BATTLE":
+                        gameType = GameType.BUILD_BATTLE;
+                        break;
+                    case "DUELS":
+                        gameType = GameType.DUELS;
+                        break;
+                    case "UHC CHAMPIONS":
+                        gameType = GameType.UHC_CHAMPIONS;
+                        break;
+                    case "THE TNT GAMES":
+                        gameType = GameType.TNT_GAMES;
+                        break;
+                    case "CLASSIC GAMES":
+                        gameType = GameType.CLASSIC_GAMES;
+                        break;
+                    case "COPS AND CRIMS":
+                        gameType = GameType.COPS_AND_CRIMS;
+                        break;
+                    case "BLITZ SG":
+                        gameType = GameType.BLITZ_SG;
+                        break;
+                    case "MEGA WALLS":
+                        gameType = GameType.MEGA_WALLS;
+                        break;
+                    case "SMASH HEROES":
+                        gameType = GameType.SMASH_HEROES;
+                        break;
+                    case "WARLORDS":
+                        gameType = GameType.WARLORDS;
                         break;
                     default:
                         gameType = GameType.UNKNOWN;
--- src/main/java/club/sk1er/hytilities/handlers/game/GameType.java
+++ src/main/java/club/sk1er/hytilities/handlers/game/GameType.java
@@ -3,5 +3,19 @@ package club.sk1er.hytilities.handlers.game;
 public enum GameType {
     UNKNOWN,
     BED_WARS,
-    SKY_WARS
+    SKY_WARS,
+    PROTOTYPE,
+    MURDER_MYSTERY,
+    HOUSING,
+    ARCADE_GAMES,
+    BUILD_BATTLE,
+    DUELS,
+    UHC_CHAMPIONS,
+    TNT_GAMES,
+    CLASSIC_GAMES,
+    COPS_AND_CRIMS,
+    BLITZ_SG,
+    MEGA_WALLS,
+    SMASH_HEROES,
+    WARLORDS
 }
