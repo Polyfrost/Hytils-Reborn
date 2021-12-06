24a753aa18d5f47553a288f00ea4370460be611b
MicrocontrollersDev <66657148+MicrocontrollersDev@users.noreply.github.com>
2021-12-05 23:40:04 -0700

    fix: grammar fixes in config / readme (#3)
    * fix: grammar fixes in config
    
    * fix: grammar fixes in readme, remove excess markdown

--- src/main/java/club/sk1er/hytilities/config/HytilitiesConfig.java
+++ src/main/java/club/sk1er/hytilities/config/HytilitiesConfig.java
@@ -122,7 +122,7 @@ public class HytilitiesConfig extends Vigilant {
 
     @Property(
         type = PropertyType.SWITCH, name = "Remove Auto Activated Quest Messages",
-        description = "Remove automatically activated quest messages",
+        description = "Remove automatically activated quest messages.",
         category = "Chat", subcategory = "Toggles"
     )
     public static boolean questsMessages;
@@ -314,7 +314,7 @@ public class HytilitiesConfig extends Vigilant {
 
     @Property(
         type = PropertyType.SWITCH, name = "Hide Armor",
-        description = "Hide armor in games where armour is always the same.",
+        description = "Hide armor in games where armor is always the same.",
         category = "Game", subcategory = "Visual"
     )
     public static boolean hideArmor;
@@ -398,7 +398,7 @@ public class HytilitiesConfig extends Vigilant {
 
     @Property(
         type = PropertyType.SWITCH, name = "Auto-Complete Play Commands",
-        description = "Allows tab completion of /play commands",
+        description = "Allows tab completion of /play commands.",
         category = "Chat", subcategory = "AutoComplete"
     )
     public static boolean autocompletePlayCommands;
