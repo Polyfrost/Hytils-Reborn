2a3e6c607dee830fac2f544d4b11fcdf090a9339
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-12-08 00:58:16 +0900

    bump mod version

--- src/main/java/net/wyvest/hytilities/Hytilities.java
+++ src/main/java/net/wyvest/hytilities/Hytilities.java
@@ -67,7 +67,7 @@ public class Hytilities {
 
     public static final String MOD_ID = "hytilities-reborn";
     public static final String MOD_NAME = "Hytilities Reborn";
-    public static final String VERSION = "1.0.0-beta13";
+    public static final String VERSION = "1.0.0-beta14";
 
     @Mod.Instance(MOD_ID)
     public static Hytilities INSTANCE;
