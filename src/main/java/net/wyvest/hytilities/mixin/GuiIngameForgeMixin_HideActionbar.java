f6bc297825279cd1f4311bed0e64343fd0836bbd
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-12-18 13:14:04 +0700

    i forgor

--- src/main/java/net/wyvest/hytilities/mixin/GuiIngameForgeMixin_HideActionbar.java
+++ src/main/java/net/wyvest/hytilities/mixin/GuiIngameForgeMixin_HideActionbar.java
@@ -28,7 +28,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiIngameForge.class)
+@Mixin(value = GuiIngameForge.class, remap = false)
 public class GuiIngameForgeMixin_HideActionbar {
     @Inject(method = "renderRecordOverlay", at = @At("HEAD"), cancellable = true)
     private void cancelActionBar(int width, int height, float partialTicks, CallbackInfo ci) {
