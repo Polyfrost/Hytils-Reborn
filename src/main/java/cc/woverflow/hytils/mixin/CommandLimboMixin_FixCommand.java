ab56308f5aec30f093ef29bd3e9965a120f50ad0
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-05-06 16:03:16 +0700

    fix autotip command manually as well

---
+++ src/main/java/cc/woverflow/hytils/mixin/CommandLimboMixin_FixCommand.java
@@ -0,0 +1,36 @@
+/*
+ * Hytils Reborn - Hypixel focused Quality of Life mod.
+ * Copyright (C) 2022  W-OVERFLOW
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package cc.woverflow.hytils.mixin;
+
+import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Pseudo
+@Mixin(targets = "me.semx11.autotip.command.impl.CommandLimbo", remap = false)
+public class CommandLimboMixin_FixCommand {
+
+    @Dynamic("AutoTip")
+    @ModifyConstant(method = "onCommand", constant = @Constant(stringValue = "/achat \u00a7c"))
+    private String fixAutotipCommand(String value) {
+        return "/achat §";
+    }
+}
