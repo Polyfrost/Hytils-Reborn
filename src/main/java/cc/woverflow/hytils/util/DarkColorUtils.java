a101bda75e1b480d064f2256bf05927aeeb7ac1d
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2022-06-21 10:09:09 +0200

    temp fix maybe

--- src/main/java/cc/woverflow/hytils/util/DarkColorUtils.java
+++ src/main/java/cc/woverflow/hytils/util/DarkColorUtils.java
@@ -18,43 +18,27 @@
 
 package cc.woverflow.hytils.util;
 
-import cc.polyfrost.oneconfig.libs.caffeine.cache.Cache;
-import cc.polyfrost.oneconfig.libs.caffeine.cache.Caffeine;
 import cc.polyfrost.oneconfig.utils.color.ColorUtils;
-import cc.woverflow.hytils.HytilsReborn;
 import cc.woverflow.hytils.config.HytilsConfig;
 
+import java.util.HashMap;
 import java.util.Objects;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class DarkColorUtils {
-    private static final AtomicInteger counter = new AtomicInteger(0);
-    private static final ThreadPoolExecutor POOL = new ThreadPoolExecutor(
-        50, 50,
-        0L, TimeUnit.SECONDS,
-        new LinkedBlockingQueue<>(), (r) -> new Thread(
-        r,
-        String.format("%s Cache Thread %s", HytilsReborn.MOD_NAME, counter.incrementAndGet())
-    )
-    );
-
-    private static final Cache<Integer, Integer> cache = Caffeine.newBuilder().executor(POOL).maximumSize(100).build();
+    private static final HashMap<Integer, Integer> cache = new HashMap<>();
 
     public static void invalidateCache() {
-        cache.invalidateAll();
+        cache.clear();
     }
 
     public static int getCachedDarkColor(int mapColor) {
-        Integer color = cache.getIfPresent(mapColor);
+        Integer color = cache.get(mapColor);
         if (color == null) {
             cache.put(mapColor, ((0xFF) << 24) |
                 ((Math.round(Math.max((float) ColorUtils.getRed(mapColor) * ((float) (1000 - HytilsConfig.overlayAmount) / 1000), 0.0F)) & 0xFF) << 16) |
                 ((Math.round(Math.max((float) ColorUtils.getGreen(mapColor) * ((float) (1000 - HytilsConfig.overlayAmount) / 1000), 0.0F)) & 0xFF) << 8) |
                 ((Math.round(Math.max((float) ColorUtils.getBlue(mapColor) * ((float) (1000 - HytilsConfig.overlayAmount) / 1000), 0.0F)) & 0xFF)));
-            return Objects.requireNonNull(cache.getIfPresent(mapColor));
+            return Objects.requireNonNull(cache.get(mapColor));
         } else {
             return color;
         }
