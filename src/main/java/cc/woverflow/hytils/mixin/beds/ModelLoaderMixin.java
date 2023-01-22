01b8af5cd0f6b314d7b78af3e6a512c461f6116f
Wyvest <45589059+Wyvest@users.noreply.github.com>
2023-01-22 03:36:08 -0500

    fix refmap not generating ModelLoaderMixin stuff

--- src/main/java/cc/woverflow/hytils/mixin/beds/ModelLoaderMixin.java
+++ src/main/java/cc/woverflow/hytils/mixin/beds/ModelLoaderMixin.java
@@ -37,7 +37,7 @@ public class ModelLoaderMixin {
     @Final
     private Set<ResourceLocation> textures;
 
-    @Inject(method = "setupModelRegistry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureMap;loadSprites(Lnet/minecraft/client/resources/IResourceManager;Lnet/minecraft/client/renderer/texture/IIconCreator;)V"))
+    @Inject(method = "setupModelRegistry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureMap;loadSprites(Lnet/minecraft/client/resources/IResourceManager;Lnet/minecraft/client/renderer/texture/IIconCreator;)V"), remap = true)
     private void getVariantsTextureLocations(CallbackInfoReturnable<Set<ResourceLocation>> cir) {
         for (String color : BedLocationHandler.COLORS_REVERSE.values()) {
             textures.add(new ResourceLocation(BedModelHook.COLORED_BED + color));
