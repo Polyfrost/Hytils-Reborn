1001e1c8516631671a445bf9313912340c38450b
mustangk <mustang.kyy@gmail.com>
2026-01-30 11:48:45 +0100

    check casualGameEndRegex setting and update regex link

--- src/main/java/org/polyfrost/hytils/handlers/chat/modules/triggers/AutoGG.java
+++ src/main/java/org/polyfrost/hytils/handlers/chat/modules/triggers/AutoGG.java
@@ -70,7 +70,10 @@ public class AutoGG implements ChatReceiveModule {
         }
 
         // TODO: UNTESTED!
-        return getLanguage().casualGameEndRegex.matcher(message).matches();
+        if (HytilsConfig.casualAutoGG) {
+            return getLanguage().casualGameEndRegex.matcher(message).matches();
+        }
+        return false;
     }
 
     @Subscribe
